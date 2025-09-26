89655917aa3f772f8d110b970d98d2c36cb43bc3
AydMaxx <aydmaxx@gmail.com>
2025-09-26 09:49:27 +0400

    chore(i18n): set env._next_intl_trailing_slash='never' to align with middleware and silence Next warning

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const withNextIntl = require('next-intl/plugin')('./src/i18n.ts')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  env: {
+    _next_intl_trailing_slash: 'never',
+  },
   async redirects() {
     return [
       { source: '/auth/login', destination: '/login', permanent: true },
